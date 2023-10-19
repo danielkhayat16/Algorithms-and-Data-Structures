e3790c4965b80347f27319b160c861a3e1b5aa00
danielkhayat8 <daniel.khayat8@gmail.com>
2023-10-19 14:59:55 +0300

    Multiple pointers problems

--- ProblemSolvingPatterns/solvingPatterns.java
+++ ProblemSolvingPatterns/solvingPatterns.java
@@ -9,8 +9,20 @@ public class solvingPatterns {
     public static void main (String[] args){
 
         int[] arr1 = {1,2,2,3,3,3, 3};
-        int [] arr2 = {1, 4, 4, 9, 9, 9};
+        int [] arr2 = {1,1, 4, 4, 9, 9, 9};
+        int [] arr3 = {-4,4,5,6,10,11,14};
         System.out.println(same(arr1, arr2));
+
+        String str1 = "Daniel";
+        String str2 = "lienaD";
+        System.out.println(validAnagram(str1, str2));
+        
+        int [] sum = sumZero(arr3);
+        System.out.println("[" + sum[0] + "," + sum[1] + "]");
+
+        int num = countUniqueValues(arr1);
+        System.out.println(num);
+        
     }
 
     // Frequency counter problem #1.
@@ -69,7 +81,81 @@ public class solvingPatterns {
     public static boolean validAnagram( String str1, String str2){
 
 
+        if(str1.length() != str2.length()) return false;
+        
+        HashMap <Character, Integer> firstStringFrequencies = new HashMap<>();
+        HashMap <Character, Integer> secondStringFrequencies = new HashMap<>();
+        firstStringFrequencies = countFreqString(str1);
+        secondStringFrequencies = countFreqString(str2);
+        
+        if(firstStringFrequencies.size() != secondStringFrequencies.size()) return false;
+
+        for (Character s : firstStringFrequencies.keySet()){
+            if(firstStringFrequencies.get(s) != secondStringFrequencies.get(s)) return false;
+        }
 
         return true;
+    }
+    
+
+    // This function helps to iterate over Strings and return a Hashmap of the frequencies.
+    private static HashMap <Character, Integer> countFreqString (String str){
+
+        HashMap <Character, Integer> stringFrequencies = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++){
+
+            if(stringFrequencies.get(str.charAt(i)) == null) stringFrequencies.put(str.charAt(i), 1);
+            else stringFrequencies.put(str.charAt(i), 1);
+        }
+
+        return stringFrequencies;
+
+    }
+
+    public static int[] sumZero (int [] arr){
+        int [] zeroSum = {0,0};
+        int high, low;
+        if(arr.length != 0){
+            low = 0;
+            high = arr.length - 1;
+        }
+        else return null;
+
+        while(low <= high)
+        {
+            if((arr[low] + arr[high]) == 0)    
+                {
+                    zeroSum[0] = arr[low];
+                    zeroSum[1] = arr[high];
+                    return zeroSum;
+                }
+            else if((arr[low] + arr[high]) < 0)
+                low ++;
+            else
+                high --;
+        }
+    
+
+        return  null;
+    }
+
+    public static int countUniqueValues (int [] arr){
+
+        if(arr.length == 0) return 0;
+        
+        int count = 1;
+        int i = 0;
+        for(int j = 1; j < arr.length; j ++){
+            if(arr[i] == arr[j]) j ++;
+            else {
+                i = j;
+                count++;
+            }
+        
+        }
+
+
+        return count;
     }
 }
